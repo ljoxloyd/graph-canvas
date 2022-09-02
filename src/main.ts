aa7a6e22cda6e4eb4036a8643da64e7f667419c5
lexlloyd <lyudskoy@gmail.com>
2022-09-02 21:24:24 +0300

    made grid move

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,81 @@
+import 'reset.css'
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import { setupCounter } from './counter'
+import { configureStore, createSlice, bindActionCreators, combineReducers } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+import { from, fromEvent, map, switchMap, takeUntil } from 'rxjs'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+/*
+======================================================
+Setup
+*/
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+const main = document.querySelector('main')!
+
+const initialGridState = {
+    position: { x: 0, y: 0 },
+    moving: false
+}
+
+type GridState = typeof initialGridState
+
+const gridSlice = createSlice({
+    name: 'grid',
+    initialState: initialGridState,
+    reducers: {
+        moveBy(state, { payload }: PayloadAction<GridState['position']>) {
+            state.position.x = (state.position.x + payload.x) % 64
+            state.position.y = (state.position.y + payload.y) % 64
+        },
+        moving(state, { payload }: PayloadAction<boolean>) {
+            state.moving = payload
+        },
+    },
+})
+
+const store = configureStore({
+    reducer: combineReducers({
+        grid: gridSlice.reducer
+    })
+})
+
+export const commit = bindActionCreators({
+    ...gridSlice.actions
+}, store.dispatch)
+
+export const store$ = from(store)
+
+namespace Effect {
+    export const updateGridPosition = (pos: GridState['position']): void => {
+        main.style.backgroundPosition = `${pos.x}px ${pos.y}px`
+    }
+}
+
+
+/*
+======================================================
+Render
+*/
+
+const mouseUp$ = fromEvent(window, "mouseup")
+const mouseDown$ = fromEvent(window, "mousedown")
+
+mouseDown$.subscribe(() => commit.moving(true))
+mouseUp$.subscribe(() => commit.moving(false))
+
+mouseDown$
+    .pipe(
+        switchMap(() =>
+            fromEvent<MouseEvent>(window, "mousemove").pipe(takeUntil(mouseUp$))
+        ),
+        map(e => ({ x: e.movementX, y: e.movementY }))
+    )
+    .subscribe(commit.moveBy);
+
+
+store$.pipe(map(state => state.grid.moving)).subscribe(is => {
+    main.style.cursor = is ? 'grabbing' : 'grab'
+})
+
+store$
+    .pipe(map(state => state.grid.position))
+    .subscribe(Effect.updateGridPosition);
