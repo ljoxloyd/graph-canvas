25229dd2395c711614d33eebefabece9208f0399
lexlloyd <lyudskoy@gmail.com>
2022-09-03 15:37:46 +0300

    added sub grid

--- src/main.ts
+++ src/main.ts
@@ -1,100 +1,122 @@
-import 'reset.css'
-import './style.css'
-import { fromEvent, map, switchMap, takeUntil } from 'rxjs'
+import type { Subscription } from 'rxjs'
+import { fromEvent, map, switchMap, takeUntil } from "rxjs";
 
 class GridCanvas {
     static attachTo(el: HTMLCanvasElement) {
-        return new GridCanvas(el)
+        return new GridCanvas(el);
     }
 
     public init() {
-        this.setupStreams()
-        this.adjustCanvasSize()
-        this.paintGrid()
-        this.setCursor('grab')
-        return this
+        this.setupEvents();
+        this.adjustCanvasSize();
+        this.setCursor("grab");
+        this.paintGrid();
+        return this;
     }
 
     private constructor(
         private readonly el: HTMLCanvasElement
     ) {
-        this.ctx = el.getContext("2d")!;
+        const maybeCtx = el.getContext("2d");
+        if (!maybeCtx) {
+            throw new Error("Couldn't get a rendering context")
+        }
+        this.ctx = maybeCtx;
     }
 
-    private readonly position = { x: 0, y: 0, }
+    private shiftX = 0;
+    private shiftY = 0;
 
-    private readonly sample = 100
+    private readonly gap = 160;
+    private readonly subgap = this.gap / 4;
 
     private readonly ctx: CanvasRenderingContext2D;
 
-    private setupStreams() {
+    private readonly subscriptions = new Array<Subscription>();
+
+    private setupEvents() {
+        let i = 0;
+        this.subscriptions.forEach(subscription => {
+            subscription.unsubscribe();
+        });
+
         const mouseDown$ = fromEvent(this.el, "mousedown");
-        mouseDown$.subscribe(() => {
-            this.setCursor('grabbing')
-        })
+        this.subscriptions[i++] = mouseDown$.subscribe(() => {
+            this.setCursor("grabbing");
+        });
 
         const mouseUp$ = fromEvent(this.el, "mouseup");
-        mouseUp$.subscribe(() => {
-            this.setCursor('grab')
-        })
+        this.subscriptions[i++] = mouseUp$.subscribe(() => {
+            this.setCursor("grab");
+        });
 
-        mouseDown$
+        this.subscriptions[i++] = mouseDown$
             .pipe(
-                switchMap(() => fromEvent<MouseEvent>(this.el, "mousemove")
-                    .pipe(takeUntil(mouseUp$))
+                switchMap(() =>
+                    fromEvent<MouseEvent>(this.el, "mousemove").pipe(takeUntil(mouseUp$))
                 ),
-                map(e => ({
-                    x: (this.position.x + e.movementX) % this.sample,
-                    y: (this.position.y + e.movementY) % this.sample,
+                map(event => ({
+                    x: (this.shiftX + event.movementX) % this.gap,
+                    y: (this.shiftY + event.movementY) % this.gap,
                 }))
             )
             .subscribe(pos => {
-                this.position.x = pos.x;
-                this.position.y = pos.y;
+                this.shiftX = pos.x;
+                this.shiftY = pos.y;
                 this.paintGrid();
             });
 
-        fromEvent(window, 'resize').subscribe(() => {
-            this.adjustCanvasSize()
-            this.paintGrid()
-        })
+        this.subscriptions[i++] = fromEvent(window, "resize").subscribe(() => {
+            this.adjustCanvasSize();
+            this.paintGrid();
+        });
     }
 
-    private paintGrid(): void {
-        const { ctx, el, position, sample } = this;
+    private paintGrid({ ctx, gap, subgap } = this): void {
+        this.clear();
 
-        ctx.fillStyle = "#fff";
-        ctx.clearRect(0, 0, el.width, el.height);
+        ctx.beginPath()
+        ctx.strokeStyle = "#d2d2d2";
+        this.drawLines(subgap);
+        ctx.stroke();
 
-        ctx.beginPath();
-        ctx.strokeStyle = "black";
-        let { x, y } = position;
-        while (x < el.width) {
-            ctx.moveTo(x, 0);
-            ctx.lineTo(x, el.height);
-            x += sample;
-        }
-        while (y < el.height) {
-            ctx.moveTo(0, y);
-            ctx.lineTo(el.width, y);
-            y += sample;
-        }
+        ctx.beginPath()
+        ctx.strokeStyle = "#2d2d2d";
+        this.drawLines(gap);
         ctx.stroke();
     }
 
-    private adjustCanvasSize() {
-        this.el.width = window.innerWidth
-        this.el.height = window.innerHeight
+    private drawLines(step: number, { ctx, el, shiftY, shiftX, gap } = this) {
+        let x = shiftX - gap;
+        let y = shiftY - gap;
+        while (x < el.width) {
+            ctx.moveTo(x, 0);
+            ctx.lineTo(x, el.height);
+            x += step;
+        }
+        while (y < el.width) {
+            ctx.moveTo(0, y);
+            ctx.lineTo(el.width, y);
+            y += step;
+        }
     }
 
-    private setCursor(value: 'grab' | 'grabbing') {
+    private clear({ ctx, el } = this) {
+        ctx.fillStyle = '#fff'
+        ctx.fillRect(0, 0, el.width, el.height);
+    }
+
+    private adjustCanvasSize({ el } = this) {
+        el.width = window.innerWidth;
+        el.height = window.innerHeight;
+    }
+
+    private setCursor(value: "grab" | "grabbing") {
         this.el.style.cursor = value;
     }
 }
 
-/*
-======================================================
-Render
-*/
-
-GridCanvas.attachTo(document.querySelector('canvas')!).init()
+const canvasEl = document.querySelector("canvas");
+if (canvasEl) {
+    GridCanvas.attachTo(canvasEl).init();
+}
